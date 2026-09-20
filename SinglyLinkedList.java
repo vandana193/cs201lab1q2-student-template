cdf46d077c19d90af48325b73111217958069ad5
vandana193 <tellvandu@hotmail.com>
2026-09-20 15:22:56 +0800

    removed Node class

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,27 +1,5 @@
 public class SinglyLinkedList<E> {
 
-    private static class Node<E> {
-        private E element;
-        private Node<E> next;
-
-        public Node(E e, Node<E> n) {
-            element = e;
-            next = n;
-        }
-
-        public E getElement() {
-            return element;
-        }
-
-        public Node<E> getNext() {
-            return next;
-        }
-
-        public void setNext(Node<E> n) {
-            next = n;
-        }
-    }
-
     private Node<E> head = null;
     private Node<E> tail = null;
     private int size = 0;
